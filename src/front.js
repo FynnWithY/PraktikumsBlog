5e61d2e1e7f4fa3d4d2d3cf44e1ffc99518cd520
Fynn <fri2007@gmx.de>
2023-01-30 22:39:28 +0100

    ahhhhhhhhhhh

--- src/front.js
+++ src/front.js
@@ -1,17 +1,23 @@
 var autocompleteSource = ["Tag 1", "Begrüßung", "Grundlagen der Informatik", "Aufgaben zu Grundlagen der Informatik", "Recap zu Grundlagen der Infromatik", "Arbeitsalltag", 
-"Tag 2", ]
+"Tag 2", "Python"]
 
 var day1=["Tag 1", "Begrüßung", "Grundlagen der Informatik", "Aufgaben zu Grundlagen der Informatik", "Recap zu Grundlagen der Infromatik", "Arbeitsalltag"]
+var day2=["Tag 2", "Python"];
 function openPage(tag)
 {
-    var url;
-    window.location.pathname= tag;
+    if(day1.includes(tag))
+    {
+        window.location.pathname= "src/day1.html";
+    }else if(day2.includes(tag))
+    {
+        window.location.pathname= "src/day2.html";
+    }
 }
 function onInput(event)
 {
-    if(key === "Enter")
+    if(event.key === "Enter")
     {
-        window.location.pathname= "src/day1.html";
+        // window.location.pathname= "src/day1.html";
         var inputValue = document.getElementById("search").value;
         console.error("The input value is: " + inputValue);
         openPage(inputValue);
