c94db6151256bb7659e1cb6ddd916d803b856e46
FynnWithY <fri2007@gmx.de>
2023-01-30 15:40:07 +0100

    Theo kann nichts

--- src/front.js
+++ src/front.js
@@ -1,2 +1,19 @@
 var autocompleteSource = ["Tag 1", "Begrüßung", "Grundlagen der Informatik", "Aufgaben zu Grundlagen der Informatik", "Recap zu Grundlagen der Infromatik", "Arbeitsalltag", 
 "Tag 2", ]
+
+var day1=["Tag 1", "Begrüßung", "Grundlagen der Informatik", "Aufgaben zu Grundlagen der Informatik", "Recap zu Grundlagen der Infromatik", "Arbeitsalltag"]
+function openPage(tag)
+{
+    var url;
+    window.location.pathname= tag;
+}
+function onInput(key)
+{
+    if(key === "Enter")
+    {
+        window.location.pathname= "src/day1";
+        var inputValue = document.getElementById("search").value;
+        console.log("The input value is: " + inputValue);
+        openPage(inputValue);
+    }
+}
