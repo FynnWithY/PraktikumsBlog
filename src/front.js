a4b840a876a4c87b1876cf42001030e9496d5139
LachCash <Theo.Daume@Inselgasse.de>
2023-01-31 09:54:24 +0100

    noch mehr begriffe

--- src/front.js
+++ src/front.js
@@ -1,10 +1,14 @@
 var autocompleteSource = ["Tag 1", "Begrüßung", "Grundlagen der Informatik", "Aufgaben zu Grundlagen der Informatik", "Recap zu Grundlagen der Infromatik", "Arbeitsalltag", 
 "Tag 2", "Python", "Python Praxis", "Python Recap", "Tung Le Trong", 
-"Tag 3", "Robotics", "IT-Sicherheit", "Sap Plattform", "Kai-Peter Schöttelndreier", "Taras Shevchenko"]
+"Tag 3", "Robotics", "IT-Sicherheit", "Sap Plattform", "Kai-Peter Schöttelndreier", "Taras Shevchenko",
+"Tag 4", "Mobile App Entwicklung", "Raffaele Chianese", 
+"Tag 5", "Agilität", "Austausch mit Auszubildenden", "Webseitenprogrammierung"]
 
 var day1=["Tag 1", "Begrüßung", "Grundlagen der Informatik", "Aufgaben zu Grundlagen der Informatik", "Recap zu Grundlagen der Infromatik", "Arbeitsalltag"]
 var day2=["Tag 2", "Python", "Python Praxis", "Python Recap", "Tung Le Trong"];
 var day3=["Tag 3", "Robotics", "IT-Sicherheit", "Sap Plattform", "Kai-Peter Schöttelndreier", "Taras Shevchenko"];
+var day4=["Tag 4", "Mobile App Entwicklung", "Raffaele Chianese"]
+var day5=["Tag 5", "Agilität", "Austausch mit Auszubildenden", "Webseitenprogrammierung"]
 function openPage(tag)
 {
     if(day1.includes(tag))
@@ -16,7 +20,13 @@ function openPage(tag)
     }else if(day3.includes(tag))
     {
         window.location.pathname="src/day3.html";
-    }    
+    }else if(day4.includes(tag))
+    {
+        window.location.pathname="src/day4.html";
+    }else if(day5.includes(tag))
+    {
+        window.location.pathname="src/day5.html";
+    }           
 }
 function onInput(event)
 {
