4cf43f2019f9409ef2bd5412fcca37007f84abe2
LachCash <Theo.Daume@Inselgasse.de>
2023-01-31 11:47:03 +0100

    amog us

--- src/front.js
+++ src/front.js
@@ -31,7 +31,7 @@ function openPage(tag)
     }else if(day6.includes(tag))
     {
         window.location.pathname= "src/day6.html";
-    }else if(tag === "http://127.0.0.1:5500/res/apple_logo.jpg")
+    }else if(tag === "http://127.0.0.1:5500/res/testLogo.png")
     {
         window.location.pathname= "src/secret.html";
     }                                            
