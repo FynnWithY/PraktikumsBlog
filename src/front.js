92c8cea545892ba2db51a8bb984f3a2fe440168d
FynnWithY <fri2007@gmx.de>
2023-02-01 09:06:07 +0100

    added the names of the day to the search bar

--- src/front.js
+++ src/front.js
@@ -1,15 +1,15 @@
-var autocompleteSource = ["Tag 1", "Begrüßung", "Grundlagen der Informatik", "Aufgaben zu Grundlagen der Informatik", "Recap zu Grundlagen der Infromatik", "Arbeitsalltag", 
-"Tag 2", "Python", "Python Praxis", "Python Recap", "Tung Le Trong", 
-"Tag 3", "Robotics", "IT-Sicherheit", "Sap Plattform", "Kai-Peter Schöttelndreier", "Taras Shevchenko",
-"Tag 4", "Mobile App Entwicklung", "Raffaele Chianese", 
-"Tag 5", "Agilität", "Austausch mit Auszubildenden", "Websitenprogrammierung", "Scrum",
+var autocompleteSource = ["Tag 1","Montag", "Begrüßung", "Grundlagen der Informatik", "Aufgaben zu Grundlagen der Informatik", "Recap zu Grundlagen der Infromatik", "Arbeitsalltag", 
+"Tag 2","Dienstag", "Python", "Python Praxis", "Python Recap", "Tung Le Trong", 
+"Tag 3","Mittwoch", "Robotics", "IT-Sicherheit", "Sap Plattform", "Kai-Peter Schöttelndreier", "Taras Shevchenko",
+"Tag 4","Donnerstag", "Mobile App Entwicklung", "Raffaele Chianese", 
+"Tag 5","Freitag","Agilität", "Austausch mit Auszubildenden", "Websitenprogrammierung", "Scrum",
 "Tag 6", "Woche 2", "Website", "Entwicklung einer eigenen Website", "Praktikumsblog"]
 
-var day1=["Tag 1", "Begrüßung", "Grundlagen der Informatik", "Aufgaben zu Grundlagen der Informatik", "Recap zu Grundlagen der Infromatik", "Arbeitsalltag"]
-var day2=["Tag 2", "Python", "Python Praxis", "Python Recap", "Tung Le Trong"];
-var day3=["Tag 3", "Robotics", "IT-Sicherheit", "Sap Plattform", "Kai-Peter Schöttelndreier", "Taras Shevchenko"];
-var day4=["Tag 4", "Mobile App Entwicklung", "Raffaele Chianese"]
-var day5=["Tag 5", "Agilität", "Austausch mit Auszubildenden", "Websitenprogrammierung", "Scrum"]
+var day1=["Tag 1", "Montag", "Begrüßung", "Grundlagen der Informatik", "Aufgaben zu Grundlagen der Informatik", "Recap zu Grundlagen der Infromatik", "Arbeitsalltag"]
+var day2=["Tag 2", "Dienstag", "Python", "Python Praxis", "Python Recap", "Tung Le Trong"];
+var day3=["Tag 3", "Mittwoch", "Robotics", "IT-Sicherheit", "Sap Plattform", "Kai-Peter Schöttelndreier", "Taras Shevchenko"];
+var day4=["Tag 4", "Donnerstag", "Mobile App Entwicklung", "Raffaele Chianese"]
+var day5=["Tag 5", "Freitag", "Agilität", "Austausch mit Auszubildenden", "Websitenprogrammierung", "Scrum"]
 var day6=["Tag 6", "Woche 2", "Website", "Entwicklung einer eigenen Website", "Praktikumsblog"]
 function openPage(tag)
 {
