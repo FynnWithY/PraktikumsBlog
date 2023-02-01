59b029d15006220e3083930b260e681cddc455ab
LachCash <Theo.Daume@Inselgasse.de>
2023-02-01 15:23:58 +0100

    kcasa

--- src/front.js
+++ src/front.js
@@ -1,9 +1,4 @@
-var autocompleteSource = ["Tag 1","Montag", "Begrüßung", "Grundlagen der Informatik", "Aufgaben zu Grundlagen der Informatik", "Recap zu Grundlagen der Infromatik", "Arbeitsalltag", 
-"Tag 2","Dienstag", "Python", "Python Praxis", "Python Recap", "Tung Le Trong", 
-"Tag 3","Mittwoch", "Robotics", "IT-Sicherheit", "Sap Plattform", "Kai-Peter Schöttelndreier", "Taras Shevchenko",
-"Tag 4","Donnerstag", "Mobile App Entwicklung", "Raffaele Chianese", 
-"Tag 5","Freitag","Agilität", "Austausch mit Auszubildenden", "Websitenprogrammierung", "Scrum",
-"Tag 6", "Woche 2", "Website", "Entwicklung einer eigenen Website", "Praktikumsblog"]
+var autocompleteSource = ["Tag 1","Montag", "Begrüßung", "Grundlagen der Informatik", "Aufgaben zu Grundlagen der Informatik", "Recap zu Grundlagen der Infromatik", "Arbeitsalltag", "Tag 2","Dienstag", "Python", "Python Praxis", "Python Recap", "Tung Le Trong", "Tag 3","Mittwoch", "Robotics", "IT-Sicherheit", "Sap Plattform", "Kai-Peter Schöttelndreier", "Taras Shevchenko","Tag 4","Donnerstag", "Mobile App Entwicklung", "Raffaele Chianese", "Tag 5","Freitag","Agilität", "Austausch mit Auszubildenden", "Websitenprogrammierung", "Scrum","Tag 6", "Woche 2", "Website", "Entwicklung einer eigenen Website", "Praktikumsblog"]
 
 var day1=["Tag 1", "Montag", "Begrüßung", "Grundlagen der Informatik", "Aufgaben zu Grundlagen der Informatik", "Recap zu Grundlagen der Infromatik", "Arbeitsalltag"]
 var day2=["Tag 2", "Dienstag", "Python", "Python Praxis", "Python Recap", "Tung Le Trong"];
